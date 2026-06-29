33e0337c75aed8aebc1a7db59d4e1eb4bf29aa32
Dexter Tingal <dexterpaultingal15062@gmail.com>
2026-06-30 05:56:23 +0800

    fix the product api to fetch the products

--- Back-End/products-api.php
+++ Back-End/products-api.php
@@ -1,6 +1,13 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: https://bake-co.vercel.app");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Handle preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require __DIR__ . '/db.php';
 
