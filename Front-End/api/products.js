c1d8c21734088a362bdc709ab20bcd70ccc3afb4
Dexter Tingal <dexterpaultingal15062@gmail.com>
2026-07-01 17:20:55 +0800

    Day2 V3 for the products to be fetch

--- Front-End/api/products.js
+++ Front-End/api/products.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   const response = await fetch('https://bake-co.infinityfree.me/Back-End/products-api.php', {
     headers: {
