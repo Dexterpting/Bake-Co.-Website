776d2de7b4cd65af049717cb9a95c4dafb54c3e1
Dexter Tingal <dexterpaultingal15062@gmail.com>
2026-07-01 17:28:20 +0800

    Day2 V4 for the product ot be fetch

--- Front-End/api/products.js
+++ Front-End/api/products.js
@@ -1,9 +1,7 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   const response = await fetch('https://bake-co.infinityfree.me/Back-End/products-api.php', {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
       'Accept': 'application/json, text/plain, */*',
       'Referer': 'https://bake-co.vercel.app',
     }
